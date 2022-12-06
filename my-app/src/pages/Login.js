f7f95b44326dc0681a3733fb06379bfdde8ee33a
Mahabur Rahman <annur4395@gmail.com>
2022-12-06 18:25:08 +0600

    something updated

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const signInWithGoogle = () => {};
+
+  return (
+    <>
+      <div className="loginPage">
+        <p>Sign in with Google to continue</p>
+
+        <button onClick={signInWithGoogle}>SignIn with Google</button>
+      </div>
+    </>
+  );
 };
 
 export default Login;
